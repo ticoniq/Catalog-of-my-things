36f4429c9c09f27efff5b39ed2ac99dee5610a42
ticoniq_ <okolietochi@gmail>
2023-09-14 09:09:30 +0100

    fix linters error

--- main.rb
+++ main.rb
@@ -25,9 +25,9 @@ class Main
     puts '1. - List all books'
     puts '2. - List all music albums'
     puts '3. - List all games'
-    puts "4. - List all genres"
-    puts "5. - List all labels"
-    puts "6. - List all authors"
+    puts '4. - List all genres'
+    puts '5. - List all labels'
+    puts '6. - List all authors'
     puts '7. - Add book'
     puts '8. - Add a music album'
     puts '9. - Add a game'
